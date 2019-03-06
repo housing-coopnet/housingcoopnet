ab4e260834bb1cefa80805e5a5a19a64648cb209
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2019-03-06 02:07:00 -0800

    Updates

---
+++ precache-manifest.054829ea80572545a9e9d9b640c72a62.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f8256eb839b1c0e505fbd15bee08abd",
+    "url": "/static/media/carousel1.6f8256eb.jpg"
+  },
+  {
+    "revision": "b026fef0f9f6809b5fbc",
+    "url": "/static/css/main.3d723862.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b53e3e3aaf9201b27813",
+    "url": "/static/js/2.b53e3e3a.chunk.js"
+  },
+  {
+    "revision": "7a100d10c4ad2e100597bf7eeb111127",
+    "url": "/static/media/Logo.7a100d10.png"
+  },
+  {
+    "revision": "db1f10e16ad910c94f6e06352ad36f4d",
+    "url": "/static/media/tile1.db1f10e1.jpg"
+  },
+  {
+    "revision": "0d2f495c39e6abd7038971c195780a02",
+    "url": "/static/media/tile2.0d2f495c.jpg"
+  },
+  {
+    "revision": "c2bac21eef1b03c170603537d2d521b4",
+    "url": "/static/media/tile3.c2bac21e.jpg"
+  },
+  {
+    "revision": "dbf70a0d6e1c900140e6091f1a918c3e",
+    "url": "/static/media/tile4.dbf70a0d.jpg"
+  },
+  {
+    "revision": "b026fef0f9f6809b5fbc",
+    "url": "/static/js/main.b026fef0.chunk.js"
+  },
+  {
+    "revision": "78332026740c8abeee194ddaeb6d05cb",
+    "url": "/static/media/carousel2.78332026.jpg"
+  },
+  {
+    "revision": "1a6a11a38864b7651e01f7e9ccd3e327",
+    "url": "/static/media/carousel3.1a6a11a3.jpg"
+  },
+  {
+    "revision": "b5dc307d47c79a124c56f0c6055d82ec",
+    "url": "/static/media/introJumboImage.b5dc307d.jpg"
+  },
+  {
+    "revision": "350c1f368954f892ec4884cf4d4f8f66",
+    "url": "/static/media/aaron.350c1f36.jpeg"
+  },
+  {
+    "revision": "9f9859b3c20f2e2296950cb92efe7b4e",
+    "url": "/static/media/ramsay.9f9859b3.jpeg"
+  },
+  {
+    "revision": "051619303d63a29a4dd058c96f0a7cec",
+    "url": "/static/media/jinsu.05161930.jpeg"
+  },
+  {
+    "revision": "9d4a8629b8ff29141753395da91765fd",
+    "url": "/static/media/cara.9d4a8629.jpeg"
+  },
+  {
+    "revision": "2482fa1b68a17ed2232f692dc9005223",
+    "url": "/index.html"
+  }
+];

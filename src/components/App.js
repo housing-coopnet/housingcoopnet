138c531d72af3deacde3ae0e7c569cf0c410ebf5
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2019-06-03 12:34:27 -0700

    landing page overhaul

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route, Link
+  Route
 } from 'react-router-dom'
 
 import Home from './Home';
@@ -9,13 +9,14 @@ import Signup from './Signup';
 import ProjectDashboard from "./ProjectDashboard";
 import About from './About';
 import CoopTalk from './CoopTalk';
+import Landing from "./Landing";
 
 export default class App extends Component {
   render() {
     return (
 <Router>
       <div>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Landing} />
         <Route path="/signup" component={Signup} />
         <Route path="/about" component={About} />
           <Route path="/cooptalk" component={CoopTalk} />
--- src/components/CarouselSlider.js
+++ src/components/CarouselSlider.js
@@ -1,59 +1,46 @@
 import React, { Component } from 'react';
 import '../stylesheets/App.css';
-import { ProgressBar, Container, Col, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Carousel, Table, Jumbotron } from 'react-bootstrap';
+import { Popover, OverlayTrigger, ProgressBar, Container, Col, Row, Navbar, Nav, NavDropdown, Form, FormControl, Button, Carousel, Table, Jumbotron } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom'
 
+const popover = (
+     <Popover id="popover-basic" title="Member profiles are coming soon!">
+          We are in the process of implementing a fully scalable and secure member registration system! Stay tuned.
+     </Popover>
+);
+
 function HomeCarousel(props) {
-    return (
-      <Carousel.Item>
-      <img
-        className="d-block w-100"
-        src={props.slide["image"]}
-        alt=""
-      />
-  
-      <Carousel.Caption>
-        <h3>{props.slide["header"]}</h3>
-        <p>{props.slide["text"]}</p>
-      </Carousel.Caption>
-      <Button className="carouselButton">{props.slide["button"]}</Button>
-    </Carousel.Item>
-    );
-  }
-  
-var carouselSlides = [
-    {
-      "key" : 1,
-      "header" : "Slide 1",
-      "text" : "Take an interest",
-      "button" : "Sign up",
-      "image" : "https://thepioneeronline.com/wp-content/uploads/2017/06/GRAFF_12-1500x998.jpg",
-    },
-    {
-      "key" : 2,
-      "header" : "Slide 2",
-      "text" : "Build a portfolio",
-      "button" : "Invest",
-      "image" : "https://ww2.kqed.org/wp-content/uploads/sites/2/2018/06/OakMural-7-1020x680.jpg",
-    },
-    {
-      "key" : 3,
-      "header" : "Slide 3",
-      "text" : "Look at our projects",
-      "button" : "Explore",
-      "image" : "https://thepioneeronline.com/wp-content/uploads/2017/06/GRAFF_10-1500x998.jpg",
-    }
-  ]
-  
-  class CarouselSlider extends Component {
-    render() {
-        return (
-            <Carousel>
-            {carouselSlides.map((slide) => <HomeCarousel slide={slide}/>)}
-          </Carousel>
+     return (
+          <Carousel.Item>
+               <img className="d-block w-100" src={require("../images/carousel1.jpg")} alt="" />
+               <Carousel.Caption>
+                    <h3>{props.slide["header"]}</h3>
+                    <p>{props.slide["text"]}</p>
+                    <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+                         <NavLink to="/">
+                              <Button className="carouselButton">{props.slide["button"]}</Button>
+                         </NavLink>
+                    </OverlayTrigger>
+               </Carousel.Caption>
+          </Carousel.Item>
      );
-    }
-    }
-    
-  export default CarouselSlider;
+}
+class CarouselSlider extends Component {
+     constructor(props) {
+          super(props);
+          // this.state = {
+          //      slides : this.props.slides
+          // };
+     }
+
+     render() {
+          return (
+                    <Carousel>
+                         {this.props.slides.map((slide) => <HomeCarousel key={slide.key} slide={slide} />)}
+                    </Carousel>
+          );
+     }
+}
+
+export default CarouselSlider;
 

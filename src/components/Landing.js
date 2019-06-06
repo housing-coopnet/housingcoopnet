7b7bcbcf9e682e796c01aa3132541112eaf478cc
JinsuElhance <33761390+JinsuElhance@users.noreply.github.com>
2019-06-05 20:03:02 -0700

    Reworked packages, fixed landing, added fullpage

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import '../stylesheets/Landing.css';
 import { Input, Image, Container, Col, Row, Button } from 'react-bootstrap';
 import firebase from "../config/fbConfig"
+import ReactFullpage from '@fullpage/react-fullpage';
 const db = firebase.firestore()
+
+
 
 class Landing extends Component {
      constructor(props) {
@@ -29,7 +32,7 @@ class Landing extends Component {
 
           this.handleSubmit = (event) => {
                let emailInput = document.getElementById("emailInput")
-               
+               let submitButton = document.getElementById("submitButton")
                if (emailIsValid(this.state.email)) {
                     var userData = db.collection("userData")
                     let query = userData.where("email", "==", this.state.email);
@@ -43,10 +46,13 @@ class Landing extends Component {
                                         console.error("Error adding document: ", error);
                                    });
                               emailInput.value = ""
-                              event.target.innerText = "DONE"
-          
+                              submitButton.innerText = "DONE"
+                              submitButton.disabled = true;
+                              emailInput.disabled = true;
+                              emailInput.style.borderColor = "#3d3d3d";
                          } else {
                               alert("This email address is already on the list")
+
                          }
                     })
                } else {
@@ -62,43 +68,54 @@ class Landing extends Component {
 
      render() {
           return (
-               <Container fluid noGutters={true}>
-                    <Row style={{ paddingTop: 100, paddingBottom: 100, paddingLeft: 75, paddingRight: 75, height: window.innerHeight - 173 }}>
-                         <Col lg>
-                              <Row className="titleContainer">
-                                   <h1> COOPNET </h1>
-                              </Row>
-                              <Row className="blurbContainer">
-                                   <p>A network of diverse cooperative communities in which everyone has a place to call home.</p>
-                              </Row>
-                         </Col>
-                         <Col className="jumbo" lg>
-                              <Image fluid rounded src={require('../images/landing.jpg')} />
-                         </Col>
-                    </Row>
-                    <Row className="horizontalBar">
-                         <h1>JOIN THE MOVEMENT THAT IS REDEFINING COOPERATIVE HOUSING</h1>
-                    </Row>
-                    <Container id = "infoContainer" style={{ height:  "100%"}} className="infoContainer" fluid>
-                         <Row fluid className="tileContainer">
-                              <Col xs style={{paddingTop: 40 }}> <Image className="tile" src={require("../images/tile1.jpg")} rounded /></Col>
-                              <Col xs style={{paddingTop: 40 }}> <Image className="tile" src={require("../images/tile2.jpg")} rounded /></Col>
-                              <Col xs style={{paddingTop: 40 }}> <Image className="tile" src={require("../images/tile3.jpg")} rounded /></Col>
-                              <Col xs style={{paddingTop: 40 }}> <Image className="tile" src={require("../images/tile4.jpg")} rounded /></Col>
-                              {/* {projectTiles.map((tile) => <ProjectTile tile={tile} />)} */}
-                         </Row>
-                         <Row className="grabber">
-                              <h1>Are you interested in joining, starting, or funding a co-op?</h1>
-                              <h1>Follow the movement with our newsletter.</h1>
-                         </Row>
-                         <Row className = "formContainer">
-                              <Col sm className="emailContainer"><input id = "emailInput" spellCheck= "false" placeholder = "Your Email" autoComplete className="emailInput" type="email" onChange={(event) => this.handleChange(event)}></input> </Col>
-                              <Col sm><Button className="submitButton" onClick={(event) => this.handleSubmit(event)}>
-                                   SIGN UP
-                         </Button></Col>
-                         </Row>
-                    </Container>
-               </Container>
+               <ReactFullpage
+                    render={({ state, fullpageApi }) => {
+                         return (
+                              <ReactFullpage.Wrapper>
+                                   {/* <Container fluid noGutters={true}> */}
+                                        <Container fluid className = "section"> 
+                                        <Row style={{ paddingTop: "3%", paddingBottom: "3%", paddingLeft: "3%", paddingRight: "3%", height: window.innerHeight - 173 }}>
+                                             <Col lg>
+                                                  <Row className="titleContainer">
+                                                       <h1> COOPNET </h1>
+                                                  </Row>
+                                                  <Row className="blurbContainer">
+                                                       <p>A network of diverse cooperative communities in which everyone has a place to call home.</p>
+                                                  </Row>
+                                             </Col>
+                                             <Col className="jumbo" lg>
+                                                  <Image fluid rounded src={require('../images/landing.jpg')} />
+                                             </Col>
+                                        </Row>
+                                        <Row className="horizontalBar">
+                                             <h1>JOIN THE MOVEMENT THAT IS REDEFINING COOPERATIVE HOUSING</h1>
+                                        </Row>
+                                   </Container>
+                                   <Container fluid className = "section">
+                                        <Container id="infoContainer" style={{ height: "100%" }} className="infoContainer" fluid>
+                                             <Row  style={{ paddingTop: "3%", paddingBottom: "3%"}} fluid className="tileContainer">
+                                                  <Col xs={6} md style={{ paddingTop: 40 }}> <Image fluid className="tile" src={require("../images/tile1.jpg")} rounded /></Col>
+                                                  <Col xs={6} md style={{ paddingTop: 40 }}> <Image fluid className="tile" src={require("../images/tile2.jpg")} rounded /></Col>
+                                                  <Col xs={6} md style={{ paddingTop: 40 }}> <Image fluid className="tile" src={require("../images/tile3.jpg")} rounded /></Col>
+                                                  <Col xs={6} md style={{ paddingTop: 40 }}> <Image fluid className="tile" src={require("../images/tile4.jpg")} rounded /></Col>
+                                                  {/* {projectTiles.map((tile) => <ProjectTile tile={tile} />)} */}
+                                             </Row>
+                                             <Row className="grabber">
+                                                  <h1>Are you interested in joining, starting, or funding a co-op?<br/>Follow the movement with our newsletter.</h1>
+                                             </Row>
+                                             <Row  className="formContainer">
+                                                  <Col className="emailContainer"><input id="emailInput" spellCheck="false" placeholder="Your Email" autoComplete className="emailInput" type="email" onChange={(event) => this.handleChange(event)}></input> </Col>
+                                                  <Col ><Button id="submitButton" className="submitButton" onClick={(event) => this.handleSubmit(event)}>
+                                                       SIGN UP
+                                             </Button></Col>
+                                             </Row>
+                                        </Container>
+                                   </Container>
+                                   {/* </Container> */}
+                              </ReactFullpage.Wrapper>
+                         );
+                    }}
+               />
           );
      }
 }
